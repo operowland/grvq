fb98c4ecf06a34738d2b3cbd8fb17df73e89ba32
operowland <operowland@gmail.com>
2025-04-10 14:03:18 +0100

    Upload files to grenecast/media folder

---
+++ grenepages/download/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- DW6 -->
+<head>
+<!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<?php $wln="grenepages download";?>
+<?php $wfn="general";?>
+<?php $issue="";?>
+<?php $TI="Download";?>
+<title><?php echo $TI; ?> | grenepages</title>
+<meta name="Description"
+content="grenepages is an electronic periodical magazine designed to bring Christ to the teeming populace through various styles of writings that touches different aspects of life and answers the questions of both believers and ‘believers-to-be’. Columnists include femi sobayo, tosin iosef kehinde, femi babalola, tobi olowookere, sanmi akintayo, wunmi falodun, ope rowland, akinwumi adeoye, adeyinka meroyi oresanya, toyin maku seth-ogungbe, joan abimbola, tosin babalola, ibukun adaramola abraham"/>
+<meta name="keywords"
+content="grenepages is an electronic periodical magazine designed to bring Christ to the teeming populace through various styles of writings that touches different aspects of life and answers the questions of both believers and ‘believers-to-be’. Columnists include femi sobayo, tosin iosef kehinde, femi babalola, tobi olowookere, sanmi akintayo, wunmi falodun, ope rowland, akinwumi adeoye, adeyinka meroyi oresanya, toyin maku seth-ogungbe, joan abimbola, tosin babalola, ibukun adaramola abraham"/>
+<link rel="stylesheet" href="download.css" type="text/css" />
+<style type="text/css">
+<!--
+.style5 {	font-family: Arial, Helvetica, sans-serif;
+	color: #FFFFFF;
+	font-size: 200%;
+	font-weight: bold;
+}
+.style9 {color: #000000}
+.style10 {
+	color: #00DD00;
+	font-style: italic;
+}
+-->
+</style>
+</head>
+<body>
+<div id="masthead">
+  <?php include("../plus/headerdgen.php"); ?>
+  </div>
+  <div id="globalNav"> <a href="../index.php">Home</a> | <a href="../about.php">About</a> | <a href="../contact.php">Contact</a> | <a href="../download/index.php">Download</a></div>
+  
+
+<div id="navBar"></div>
+<!--end navBar div -->
+<div id="headlines">
+  <h3>
+    <?php include("../plus/shared.php"); ?>
+    Download the <span class="style10">grenepages</span> android app
+	<div align="center"><a href="../download/grenepages.apk"><img src="../images/chp5kf6g5s.png" alt="grenepages_android_app" width="100" height="100" border="0" /></a></div>
+  </h3>
+  <div id="advert"></div>
+</div>
+<!--end headlines -->
+<div id="content">
+  <div class="feature">
+    <h3><span class="style5">Download <span class="style10">grenepages</span> for your device </span></h3>
+  </div>
+  <div class="story">
+    
+    
+   <?php include("downloadlist.php"); ?>
+   
+
+
+
+	
+	
+    <p class="style9">&nbsp;</p>
+  </div>
+  
+</div>
+<!--end content -->
+<div id="siteInfo">
+  <?php include("../plus/footerd.php"); ?>
+</a></div>
+<br />
+</body>
+</html>
